7f048b1fe6c7489103b67fe9869e5f16f1f0a1d0
matthew nolan-cintron <matthewnolancintron@gmail.com>
2023-05-12 18:42:29 -0400

    adding doesn't rely on localStorage and instead uses firestore

--- src/view scripts/addTodoToList_view.js
+++ src/view scripts/addTodoToList_view.js
@@ -1,50 +1,28 @@
 export { addTodoToList };
 import { saveTodoToSavedListsArray } from './saveTodoToSavedListsArray_view.js';
+import { updateUserFireStoreData } from '../updateUserFireStoreData.js';
 
-/**
- * add a todo to the todo area
- * and saves it to the active list by default
- */
-function addTodoToList(todoAndListData) {
-  console.log(todoAndListData, 'todo and list data');
 
+async function addTodoToList(todoAndListData) {
   let todo = todoAndListData[0];
   let list = todoAndListData[1];
 
   /**
-  save todo to list if none specified default is active list
-  but if specified in the place in list option then move to that
-  list.
-   */
-  console.log(todo, 'todo');
-  console.log(list, 'list')
+    save todo to list if none specified default is active list
+    but if specified in the place in list option then move to that
+    list.
+  */
 
   //save todo to list in savedLists array in local storage
   saveTodoToSavedListsArray(todo, list.value);
 
   //if list.value is equal to the active list  
-  //figure out contional expression
-  console.log(list.classList.contains("todo-list_activeList"),'list active?');
-  console.log(list.classList,'classList?');
+  let savedLists = await updateUserFireStoreData('list', 'retriveListData');
+  let idOfList = savedLists[list.value]['todoListData'].uuid
+  let listElement = document.getElementById(idOfList);
 
-  /**
-   * 
-   */
-   let savedLists = JSON.parse(localStorage.getItem('savedLists'));
-   let idOfList = savedLists[list.value][0].uuid
-   console.log(idOfList,'id of list');
-   let listElement = document.getElementById(idOfList);
-   console.log(listElement,"listElement");
-
-  if(listElement.classList.contains("todo-list_activeList")){
+  if (listElement.classList.contains("todo-list_activeList")) {
     //add todo to the todo area
     document.getElementsByClassName("todo-items")[0].append(todo);
   }
-  //if not then updateTodoInTodoArea will handle it when user select another list to be active
-
-  // let savedLists = JSON.parse(localStorage.getItem('savedLists'));
-
-  // if (savedLists[list.value][0].classList.contains("todo-list_activeList")) {
-  //   document.getElementsByClassName("todo-items")[0].prepend(todo);
-  // }
 }
