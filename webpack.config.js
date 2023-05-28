5eec62a2175086d15afdd0fd4327c35aa36442f1
Matthew Nolan-Cintron <matthewnolancintron@gmail.com>
2023-05-28 14:32:43 -0400

    Update webpack.config.js
    Update webpack configuration for production and development modes

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,26 @@
 const path = require('path');
-		
-	module.exports = {
-			mode:"development",
-			devtool:'eval-source-map',
-			entry:'./src/index.js',
-			output:{
-			filename:'main.js',
-			path:path.resolve(__dirname,'dist'),
-			assetModuleFilename: 'main.js[query]',
-			scriptType: 'text/javascript',
-			publicPath: '/',
-			},
-			//for css loader (check docs for command)
-			module:{
-			rules:[
-				{
-					test:/\.css$/i,
-					use:['style-loader','css-loader'],
-				},
-			],
-		},
-	};
-	  
+
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === 'production';
+
+  return {
+    mode: isProduction ? 'production' : 'development',
+    devtool: isProduction ? 'source-map' : 'eval-source-map',
+    entry: './src/index.js',
+    output: {
+      filename: 'main.js',
+      path: path.resolve(__dirname, 'dist'),
+      assetModuleFilename: 'main.js[query]',
+      scriptType: 'text/javascript',
+      publicPath: '/',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+      ],
+    },
+  };
+};
